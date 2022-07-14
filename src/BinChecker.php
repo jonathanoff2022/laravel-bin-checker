7f97a6c29a3f0bee0ad7bd9942c108e9bd7459ff
remi <remi>
2022-07-14 18:39:59 +0200

    Change the API used. Now it is a more performant API.

--- src/BinChecker.php
+++ src/BinChecker.php
@@ -8,6 +8,9 @@ use JetBrains\PhpStorm\ArrayShape;
 
 class BinChecker
 {
+    private const DESCRIPTION_PATTERN = '<meta property="og:description" content="This number: ([0-9]{6}) is a valid BIN number ([A-Z ]+) issued by ([A-Za-z0-9 ]+) in ([A-Z ]+)">';
+    private const LEVEL_PATTERN = '<tr>\\s*<td\\s+width=\"[0-9]{1,3}%\"\\s+class=\"p-2 font-medium\">\\s*Card Level\\s*</td>\\s*<td width=\"[0-9]{1,3}%\" class=\"p-2\">\\s*([A-Z]+)\\s*</td>\\s*</tr>';
+
     /**
      * Get BIN informations
      *
@@ -15,33 +18,32 @@ class BinChecker
      * @noinspection PhpUnused The method will be used by projects. It is not meant to be used in the library
      */
     #[ArrayShape([
-        "bin" => "string", "vendor" => "string", "type" => "string", "level" => "string", "bank" => "string",
-        "country" => "string"
+        "bin" => "string", "level" => "string", "bank" => "string", "country" => "string"
     ])] public static function checkBin(string $bin): array
     {
         try {
-            $response = Http::get('https://bin-check-dr4g.herokuapp.com/api/'.$bin)->throw();
+            $response = Http::get('https://bincheck.io/details/'.$bin)->throw();
         } catch (Exception $exception) {
             throw new BinCheckerException("Http request to the bin checker API failed", 0, $exception);
         }
 
         try {
-            $result = $response->json();
-            if (!$result["result"]) {
-                throw new BinCheckerException("Bin checker API failed. Response : `".$response->body()."`");
+            if (!preg_match(self::DESCRIPTION_PATTERN, $response->body(), $descriptionMatch)) {
+                throw new BinCheckerException("Failed to match description.");
+            }
+            if (!preg_match(self::LEVEL_PATTERN, $response->body(), $levelMatch)) {
+                throw new BinCheckerException("Failed to match level.");
             }
 
-            if ($result["data"]["bin"] != $bin) {
-                throw new BinCheckerException("Returned bin is not the same. Returned=".$result["data"]["bin"].", expected=".$bin);
-            }
+            $level = $levelMatch[1];
+            $bank = $descriptionMatch[3];
+            $country = $descriptionMatch[4];
 
             return [
                 "bin" => $bin,
-                "vendor" => $result["data"]["vendor"],
-                "type" => $result["data"]["type"],
-                "level" => $result["data"]["level"],
-                "bank" => $result["data"]["bank"],
-                "country" => $result["data"]["country"]
+                "level" => $level,
+                "bank" => $bank,
+                "country" => $country
             ];
         } catch (Exception $exception) {
             throw new BinCheckerException("Unknown error occurred while checking.", 0, $exception);
