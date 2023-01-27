c4abf3a4efb5df21b1d83ccf44c961a7bdf15226
evildelta <evildelta@proton.me>
2023-01-27 23:51:48 +0100

    Update tests

--- tests/BinCheckerTest.php
+++ tests/BinCheckerTest.php
@@ -5,10 +5,23 @@ use PHPUnit\Framework\TestCase;
 
 final class BinCheckerTest extends TestCase
 {
-    public function testNatixis(): void
+    public function testNatixisGold(): void
     {
-        $natixisBin = '499010';
-        $result = BinChecker::checkBin($natixisBin);
+        $bin = '499010';
+        $result = BinChecker::checkBin($bin);
+        $this->assertEquals($bin, $result['bin'], 'Invalid bin received');
         $this->assertEquals('NATIXIS', $result['bank'], 'Invalid bank received');
+        $this->assertEquals('GOLD', $result['level'], 'Invalid level received');
+        $this->assertEquals('FR', $result['country'], 'Invalid country received');
+    }
+
+    public function testLaBanquePostaleClassic(): void
+    {
+        $bin = '497040';
+        $result = BinChecker::checkBin($bin);
+        $this->assertEquals($bin, $result['bin'], 'Invalid bin received');
+        $this->assertEquals('LA BANQUE POSTALE', $result['bank'], 'Invalid bank received');
+        $this->assertEquals('CLASSIC', $result['level'], 'Invalid level received');
+        $this->assertEquals('FR', $result['country'], 'Invalid country received');
     }
 }
